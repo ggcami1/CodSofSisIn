72cc8dfbdf722c6f7b595570fc9bb46687e97f76
camig <camigaglez.09@gmail.com>
2025-02-25 22:51:22 -0600

    nuevas practicas

--- Practica8.py
+++ Practica8.py
@@ -6,6 +6,20 @@
 #Nueva York		Estatua de la Libertad
 #Rio de Janeiro	Cristo Redentor
 
+ciudades=["Delhi","Paris","Nueva York","Rio de Janeiro"]
+monumentos=["Red Fort","Torre Eifel","Estatua de la libertad","Cristo Redentor"]
+nombre_ciudad=input("Escribe el nombre de la ciudad del que quieres saber su monumento mas famoso: ")
+encontrado = False
+y = 0;
+for x in ciudades:
+    if(nombre_ciudad==x):
+        print(monumentos[y])
+        encontrado=True
+        break
+    else:
+        y += 1
+if encontrado is False:
+    print("No hay registro de el monumento de esa ciudad")
 #-------------------------------------------------------------------------#
 #Escriba un programa para verificar si una persona puede votar o no
 
